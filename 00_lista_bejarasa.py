13b52273ba5b0013b5b6e6cf6f51f61cd1b3cc93
Tege1337 <toth-nador.tege@diak.szbi-pg.hu>
2024-11-20 13:23:36 +0100

    update

--- 00_lista_bejarasa.py
+++ 00_lista_bejarasa.py
@@ -1,10 +1,16 @@
 # gyümölcsök kiíratása
 gyumolcsok = ['alma', 'körte', 'szilva', 'barack']
 
+for x in gyumolcsok:
+    print(x)
+
 print()
 
 # hónapok kiíratása
 honapok = ['január', 'február','március', 'április', 'május', 'június', 'július'] 
+
+for x in honapok:
+    print(x)
 
 print()
 
@@ -12,8 +18,18 @@ print()
     # 0 január
     # 1 február
 
+index = 1
+for x in honapok:
+    print(f"{index} - {x}")
+    index += 1
+
 print()
 
 # index felhasználása sorszámok megadásához:
     # 1. január
     # 2. február
+
+index = 1
+for x in honapok:
+    print(f"{index}. {x}")
+    index += 1
--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,12 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+
+nums = [5, 8, 12, 15, 22]
+
+nums_num = 0
+for x in nums:
+    nums_num += 1
+
+print(nums_num) # with the for cycle
+print(len(nums)) # without the for cycle
